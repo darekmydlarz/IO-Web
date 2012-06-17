7b214d682dff0e41bebbe55ad2d6b5e2fc07d0b2
Dariusz Mydlarz <dmydlarz@gmail.com>
2012-06-17 20:16:59 +0200

    Graph changes

--- web_dispatch/canvas/js/graph.js
+++ web_dispatch/canvas/js/graph.js
@@ -1,15 +1,75 @@
-function Node(context, x, y, radius, color, text) {
+var canvas;
+var context;
+var cities = [];
+var paths = [];
+var holons = [];
+var simTimes;
+var simTimeNum = 0;
+var radius = 6;
+var holonColor = '#0000ff';
+var pathColor = '#00ff00';
+
+// parsing xml in order to draw holons locations
+jQuery.fn.parseXml = function(){
+	$.ajax({
+		type: "GET",
+		url: "./lc101.xls.xml",
+		dataType: "xml",
+		success: function(xml) {
+			if(typeof simTimes == "undefined" || simTimes.length == 0)
+				simTimes = $(xml).find('simmulation simTime');
+			simTimeNum = simTimeNum % simTimes.length;
+			$(simTimes[simTimeNum]).find('holons holon').each(function(index) {
+				var x = parseFloat($(this).attr('locationX')) * (canvas.width / 100);
+				var y = parseFloat($(this).attr('locationY')) * (canvas.height / 100);
+				var id = $(this).attr('id');
+				if(typeof holons[id] == 'undefined') {
+					holons[id] = new Node(x, y, radius, holonColor, id);
+					paths[id] = new Path(x, y, pathColor);
+				} else {
+					holons[id].moveTo(x, y);
+					paths[id].add(x, y);
+				}
+			});
+			// redraw holons positions
+			drawGraph();
+		}
+	});
+};
+
+function Path(x, y, color) {
+	this.color = color;
+	this.path = [[x, y]];
+	
+	this.draw = function() {
+		context.beginPath();
+		var path = this.path;
+		context.moveTo(path[0][0], path[0][1]);
+		var pathLength = path.length;
+		for(var i = 0; i < pathLength; ++i)
+			context.lineTo(path[i][0], path[i][1]);
+		context.strokeStyle = this.color;
+		context.stroke();
+		context.closePath();		
+	}
+	
+	this.add = function(x, y) {
+		this.path.push([x, y]);
+	}
+}
+
+function Node(x, y, radius, color, text, connectNodeIndex) {
 	this.color = color;
 	this.radius = radius;
 	this.x = x;
 	this.y = y;
 	this.text = text;
-	this.context = context;
+	this.connectNodeIndex = connectNodeIndex;	// used to draw map
 	
 	this.draw = function() {
 		context.fillStyle = color;
 		context.beginPath();
-		context.arc(x, y, radius, 0, 2 * Math.PI, false);
+		context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
 		context.closePath();
 		context.fill();
 		if(typeof(text) !== 'undefined') {
@@ -17,7 +77,7 @@ function Node(context, x, y, radius, color, text) {
 			context.fillStyle = "#000000";
 			context.textAlign = "center";
 			context.textBaseline = "middle";
-			context.fillText(text, x, y + 1.5 * radius);
+			context.fillText(text, this.x, this.y + 1.5 * radius);
 		}
 	}
 	
@@ -28,7 +88,7 @@ function Node(context, x, y, radius, color, text) {
 	
 	this.connectWith = function(node, color) {
 		context.beginPath();
-		context.moveTo(x, y);
+		context.moveTo(this.x, this.y);
 		context.lineTo(node.x, node.y);
 		context.strokeStyle = color;
 		context.stroke();
@@ -36,55 +96,51 @@ function Node(context, x, y, radius, color, text) {
 	}
 }
 
-
-var canvas;
-var context;
-var cities = [];
-	
-jQuery.fn.parseXml = function(){
-	var radius = 6;
-	var color = '#0000ff';
-	$.ajax({
-		type: "GET",
-		url: "./lc101.xls.xml",
-		dataType: "xml",
-		success: function(xml) {
-			$(xml).find('simmulation simTime').first().each(function() {
-				$(this).find('holons holon').each(function(index) {
-					var x = parseFloat($(this).attr('locationX')) * (canvas.width / 100);
-					var y = parseFloat($(this).attr('locationY')) * (canvas.height / 100);
-					var id = $(this).attr('id');
-					cities.push(new Node(context, x + 10, y + 10, radius, color, id));
-				});
-				
-			});
-		}
-	});
-};
-
-$().ready(function(){ 
+var drawMap = function() {
 	canvas = document.getElementById("myCanvas");
-	context = canvas.getContext("2d");	
+	context = canvas.getContext("2d");
+		
+	for(var i = 0; i < cities.length; ++i)
+		cities[i].connectWith(cities[cities[i].connectNodeIndex], '#EFEFEF');
+	
+	for(var city in cities)
+		cities[city].draw();
+		
+	for(var path in paths)
+		paths[path].draw();
+}
 
+var drawGraph = function () {
+	for(holon in holons)
+		holons[holon].draw();
+}
 
-	$.getJSON('js/map.js',function(data){
+var init = function(filepath) {	
+	$.getJSON(filepath,function(data){
 		var radius = 3;
-		var color = '#ff0000';
-		var currentLength = cities.length;
 		$.each(data, function(index){
 			this.x *= (canvas.width) / 100;
 			this.y *= (canvas.height) / 100;
-			cities.push(new Node(context, this.x + 10, this.y + 10, radius, color, index));
+			color = this.from > 0 ? '#ff0000' : '#929292';
+			var connectNodeIndex = this.from > 0 ? this.from : this.to;
+			cities.push(new Node(this.x, this.y, radius, color, index, connectNodeIndex));
 		});
-		
-		for(var i = currentLength; i < cities.length; ++i)
-			cities[i].connectWith(cities[0], '#EFEFEF');
-		
-		for(city in cities)
-			cities[city].draw();
 	});
+}
 
+var nextSimTime = function() {
+	canvas = document.getElementById("myCanvas");
+	context = canvas.getContext("2d");
+	context.clearRect (0 , 0 , canvas.width , canvas.height);
+	++simTimeNum;		// global variable
+	drawMap();
+	jQuery(document).parseXml();
+}
 
-
-    
+$().ready(function(){ 
+	init("./js/map.js");
+	drawMap();
+	jQuery(document).parseXml();
+	
+	setInterval("nextSimTime()", 14);
 });
