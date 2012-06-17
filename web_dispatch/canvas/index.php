a82ecc59b3fbf95261213c99fee65185738a18ab
Dariusz Mydlarz <dmydlarz@gmail.com>
2012-06-17 20:16:59 +0200

    Graph changes

--- web_dispatch/canvas/index.php
+++ web_dispatch/canvas/index.php
@@ -5,10 +5,10 @@
 <html>
 	<head>
 		<link rel='stylesheet' type='text/css' href='./css/style.css' />
-		<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'></script>
+<!--		<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'></script> -->
+		<script type='text/javascript' src='./js/jquery.js'></script>
 		<script type='text/javascript' src='./js/graph.js'></script>
 		<meta http-equiv="Access-Control-Allow-Origin" content="*"> 
-		<script type='text/javascript'>jQuery(document).parseXml();</script>
 	</head>
 	<body>
 		<canvas id="myCanvas" width="1800" height="800"></canvas>
